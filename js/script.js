9780681de15b4203c8a6d3ca15e2df375c0ce2dc
Arina <panda_in_the_fog@mail.ru>
2022-11-22 17:53:49 +0500

    Added a check for the length of the title of the movie added to the list.

--- js/script.js
+++ js/script.js
@@ -23,10 +23,14 @@ document.addEventListener('DOMContentLoaded', () => {
     addForm.addEventListener('submit', (event) => {
         event.preventDefault();
 
-        const newFilm = addInput.value;
+        let newFilm = addInput.value;
         const favorite = checkbox.checked;
 
         if (newFilm) {
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
+            
             movieDB.movies.push(newFilm);
             sortArr(movieDB.movies);
 
